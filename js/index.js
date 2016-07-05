52ab6c0e4fc5ed4daad1fc718bb3fc78cd310077
hsiw <xanizl@me.com>
2016-07-05 09:49:13 -0400

    PUSGASDa

--- js/index.js
+++ js/index.js
@@ -1,30 +1,22 @@
+// IE cannot apply CSS transforms on SVG elements. (See https://connect.microsoft.com/IE/feedback/details/811744/ie11-bug-with-implementation-of-css-transforms-in-svg)
+
+var path = anime.path('path');
+
+anime({
+  targets: 'div',
+  translateX: path,
+  translateY: path,
+  rotate: path,
+  duration: 3000,
+  loop: true,
+  easing: 'linear'
+});
+
 anime({
   targets: 'path',
-  strokeDashoffset: function(el) {
-    var pathLength = el.getTotalLength();
-    el.setAttribute('stroke-dasharray', pathLength);
-    return [-pathLength, 0];
-  },
-  stroke: {
-    value: function(el, i) {
-      return 'rgb(200,'+ i * 8 +',150)'; 
-    },
-    easing: 'linear',
-    duration: 2000,
-  },
-  strokeWidth: {
-    value: 6,
-    easing: 'linear',
-    delay: function(el, i) { 
-      return 1200 + (i * 40); 
-    },
-    duration: 800,
-  },
-  delay: function(el, i) { 
-    return i * 60; 
-  },
-  duration: 1200,
-  easing: 'easeOutExpo',
+  opacity: 0,
+  duration: 6000,
   loop: true,
-  direction: 'alternate'
+  direction: 'alternate',
+  easing: 'easeInOutExpo'
 });
